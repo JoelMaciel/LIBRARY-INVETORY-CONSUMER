6dd2ed0368e7feb83143afea33091132d063d343
Joel <joel.viana@audora.com.br>
2024-04-27 12:02:05 -0300

    first commit

---
+++ src/test/java/com/joelmaciel/libraryconsumer/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelmaciel.libraryconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
